d9e4fe1e0ae49f203a15af0a38a98f28c56b8053
Remo Inderbitzin <remo.inderbitzin@siemens.com>
2017-02-16 14:55:36 +0100

    fix: switch to docker volumes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-docker-compose up -d ovpn-data
+if [ "$#" -ne 1 ]; then
+	echo "Usage: ./setup.sh [OVPN_HOSTNAME]"
+	exit 1
+fi
+
+export OVPN_HOSTNAME=$1
+
 docker-compose run --rm genconfig
 docker-compose run --rm initpki
 docker-compose up -d openvpn-server
